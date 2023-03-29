1030cea452b8b3d7e92beb0a73e2c911da12b80b
Phoenixfirewingz <lukethemodded@gmail.com>
2023-03-29 15:37:23 +0100

    Windows - start work on a Visual Studio Extention

---
+++ plugins/LunaScriptEXT/src/Client/LunaScriptClassifier.cs
@@ -0,0 +1,89 @@
+﻿namespace LunaScriptEXT.src.Client
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Text.Classification;
+    using Microsoft.VisualStudio.Text.Tagging;
+    using Microsoft.VisualStudio.Utilities;
+    using LunaScript.Share;
+
+    [Export(typeof(ITaggerProvider))]
+    [ContentType("LunaScript")]
+    [TagType(typeof(ClassificationTag))]
+    internal sealed class LunaScriptClassifierProvider : ITaggerProvider
+    {
+
+        [Export]
+        [Name("LunaScript")]
+        [BaseDefinition("code")]
+        internal static ContentTypeDefinition LunaScriptContentType = null;
+
+        [Export]
+        [FileExtension(".lls")]
+        [ContentType("LunaScript")]
+        internal static FileExtensionToContentTypeDefinition LunaScriptFileType = null;
+
+        [Import]
+        internal IClassificationTypeRegistryService ClassificationTypeRegistry = null;
+
+        [Import]
+        internal IBufferTagAggregatorFactoryService aggregatorFactory = null;
+
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+        {
+            ITagAggregator<LunaScriptTag> tagAggregator = aggregatorFactory.CreateTagAggregator<LunaScriptTag>(buffer);
+            return new LunaScriptClassifier(tagAggregator, ClassificationTypeRegistry) as ITagger<T>;
+        }
+    }
+
+    internal sealed class LunaScriptClassifier : ITagger<ClassificationTag>
+    {
+        readonly ITagAggregator<LunaScriptTag> aggregator;
+        readonly IDictionary<Constants.WORD_TYPE, IClassificationType> LunaScriptTypesDictionary = new Dictionary<Constants.WORD_TYPE, IClassificationType>();
+        internal LunaScriptClassifier(ITagAggregator<LunaScriptTag> tagAggregator, IClassificationTypeRegistryService typeService)
+        {
+            aggregator = tagAggregator;
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.VOID] = typeService.GetClassificationType("void");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.INT8] = typeService.GetClassificationType("int8");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.INT16] = typeService.GetClassificationType("int16");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.INT32] = typeService.GetClassificationType("int32");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.INT64] = typeService.GetClassificationType("int64");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.UINT8] = typeService.GetClassificationType("uint8");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.UINT16] = typeService.GetClassificationType("uint16");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.UINT32] = typeService.GetClassificationType("uint32");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.UINT64] = typeService.GetClassificationType("uint64");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.FLOAT32] = typeService.GetClassificationType("float32");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.FLOAT64] = typeService.GetClassificationType("float64");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.FUNC] = typeService.GetClassificationType("func");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.PUBLIC] = typeService.GetClassificationType("public");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.RET] = typeService.GetClassificationType("ret");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.AND] = typeService.GetClassificationType("and");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.OR] = typeService.GetClassificationType("or");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.MOD] = typeService.GetClassificationType("mod");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.XOR] = typeService.GetClassificationType("xor");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.GLOBAL] = typeService.GetClassificationType("global");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.IMPORT] = typeService.GetClassificationType("import");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.MODULE] = typeService.GetClassificationType("module");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.SUPER] = typeService.GetClassificationType("->");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.L_SQUIGGLY] = typeService.GetClassificationType("{");
+            LunaScriptTypesDictionary[Constants.WORD_TYPE.R_SQUIGGLY] = typeService.GetClassificationType("}");
+        }
+
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged
+        {
+            add { }
+            remove { }
+        }
+
+        public IEnumerable<ITagSpan<ClassificationTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+            foreach (var tagSpan in aggregator.GetTags(spans))
+            {
+                var tagSpans = tagSpan.Span.GetSpans(spans[0].Snapshot);
+                yield return new TagSpan<ClassificationTag>(tagSpans[0], new ClassificationTag(LunaScriptTypesDictionary[tagSpan.Tag.type]));
+            }
+        }
+    }
+}
---
+++ plugins/LunaScriptEXT/src/Client/LunaScriptTagger.cs
@@ -0,0 +1,94 @@
+﻿namespace LunaScriptEXT.src.Client
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Text.Tagging;
+    using Microsoft.VisualStudio.Utilities;
+
+    [Export(typeof(ITaggerProvider))]
+    [ContentType("LunaScript")]
+    [TagType(typeof(LunaScriptTag))]
+    internal sealed class LunaScriptTagProvider : ITaggerProvider
+    {
+
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+        {
+            return new LunaScriptTagger(buffer) as ITagger<T>;
+        }
+    }
+
+    public class LunaScriptTag : ITag
+    {
+        public LunaScript.Share.Constants.WORD_TYPE type { get; private set; }
+        public LunaScriptTag(LunaScript.Share.Constants.WORD_TYPE type_in)
+        {
+            type = type_in;
+        }
+    }
+
+    internal sealed class LunaScriptTagger : ITagger<LunaScriptTag>
+    {
+
+        ITextBuffer buffer;
+        readonly IDictionary<string, LunaScript.Share.Constants.WORD_TYPE> types = new Dictionary<string, LunaScript.Share.Constants.WORD_TYPE>();
+
+        internal LunaScriptTagger(ITextBuffer buffer_in)
+        {
+            buffer = buffer_in;
+            types["void"] = LunaScript.Share.Constants.WORD_TYPE.VOID;
+            types["int8"] = LunaScript.Share.Constants.WORD_TYPE.INT8;
+            types["int16"] = LunaScript.Share.Constants.WORD_TYPE.INT16;
+            types["int32"] = LunaScript.Share.Constants.WORD_TYPE.INT32;
+            types["int64"] = LunaScript.Share.Constants.WORD_TYPE.INT64;
+            types["uint8"] = LunaScript.Share.Constants.WORD_TYPE.UINT8;
+            types["uint16"] = LunaScript.Share.Constants.WORD_TYPE.UINT16;
+            types["uint32"] = LunaScript.Share.Constants.WORD_TYPE.UINT32;
+            types["uint64"] = LunaScript.Share.Constants.WORD_TYPE.UINT64;
+            types["float32"] = LunaScript.Share.Constants.WORD_TYPE.FLOAT32;
+            types["float64"] = LunaScript.Share.Constants.WORD_TYPE.FLOAT64;
+            types["func"] = LunaScript.Share.Constants.WORD_TYPE.FUNC;
+            types["public"] = LunaScript.Share.Constants.WORD_TYPE.PUBLIC;
+            types["ret"] = LunaScript.Share.Constants.WORD_TYPE.RET;
+            types["and"] = LunaScript.Share.Constants.WORD_TYPE.AND;
+            types["or"] = LunaScript.Share.Constants.WORD_TYPE.OR;
+            types["mod"] = LunaScript.Share.Constants.WORD_TYPE.MOD;
+            types["xor"] = LunaScript.Share.Constants.WORD_TYPE.XOR;
+            types["global"] = LunaScript.Share.Constants.WORD_TYPE.GLOBAL;
+            types["import"] = LunaScript.Share.Constants.WORD_TYPE.IMPORT;
+            types["module"] = LunaScript.Share.Constants.WORD_TYPE.MODULE;
+            types["->"] = LunaScript.Share.Constants.WORD_TYPE.SUPER;
+            types["{"] = LunaScript.Share.Constants.WORD_TYPE.L_SQUIGGLY;
+            types["}"] = LunaScript.Share.Constants.WORD_TYPE.R_SQUIGGLY;
+        }
+
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged
+        {
+            add { }
+            remove { }
+        }
+
+        public IEnumerable<ITagSpan<LunaScriptTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+            foreach (SnapshotSpan curSpan in spans)
+            {
+                ITextSnapshotLine containingLine = curSpan.Start.GetContainingLine();
+                int curLoc = containingLine.Start.Position;
+                string[] tokens = containingLine.GetText().ToLower().Split(' ');
+
+                foreach (string t in tokens)
+                {
+                    if (types.ContainsKey(t))
+                    {
+                        var tokenSpan = new SnapshotSpan(curSpan.Snapshot, new Span(curLoc, t.Length));
+                        if (tokenSpan.IntersectsWith(curSpan))
+                            yield return new TagSpan<LunaScriptTag>(tokenSpan, new LunaScriptTag(types[t]));
+                    }
+                    curLoc += t.Length + 1;
+                }
+            }
+
+        }
+    }
+}
